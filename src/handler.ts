bc9cdf338317f10ea4b1d6e9d337db51e9a19052
amitrke <amitrke@gmail.com>
2019-10-02 18:09:02 -0400

    Fixing CORS issues

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,7 @@ import { buildSchemaSync } from "type-graphql";
 import { PostResolver } from "./graphql/postResolver";
 import { UserResolver } from "./graphql/userResolver";
 import { FileResolver } from "./graphql/fileResolver";
+import { Context, Callback } from "aws-lambda";
 
 console.log(`GLOBAL: ${JSON.stringify((<any>global).cachedSchema)}`);
 (<any>global).cachedSchema =
@@ -15,4 +16,28 @@ console.log(`GLOBAL: ${JSON.stringify((<any>global).cachedSchema)}`);
 const schema = (<any>global).cachedSchema;
 
 const server = new ApolloServer({ schema });
-exports.graphqlHandler = server.createHandler({cors: {origin: '*', credentials: true}});
+exports.graphqlHandler = server.createHandler(
+    {
+        cors: {
+            origin: "*",
+            methods: "OPTIONS,GET,POST",
+            allowedHeaders: "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
+        }
+    }
+);
+
+exports.optionsHandler = (event: any, context: Context, callback: Callback) => {
+    const response = {
+        statusCode: 200,
+        headers: {
+            "access-control-allow-headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent",
+            "access-control-allow-methods": "OPTIONS,GET,POST",
+            "access-control-allow-origin": "*",
+            "access-control-allow-credentials": "false"
+        },
+        body: JSON.stringify({
+          message: Math.floor(Math.random() * 10)
+        })
+      };
+    callback(undefined, response);
+}
